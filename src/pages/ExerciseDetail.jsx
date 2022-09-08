68cf6524159f9e2a354754867355894e46cb31ca
jahiker <rojasjahiker@gmail.com>
2022-09-08 00:47:42 -0400

    [add] exercise detail on detail page

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import { Box } from '@mui/material'
+
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
+
+import { exercisesOptions, fetchData } from "../utils/fetchData";
 
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({})
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com/';
+
+      const exerciseDeatilData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exercisesOptions);
+      
+      setExerciseDetail(exerciseDeatilData);
+
+    }
+
+    fetchExercisesData();
+  }, [id]);
+
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
   )
 }
 
